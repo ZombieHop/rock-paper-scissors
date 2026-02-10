62c676fa7d30059c1ad6e7451c7e3fc85a255f6a
ZombieHop <247022453+ZombieHop@users.noreply.github.com>
2026-02-10 11:52:53 +0000

    write getComputerChoice function

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-function getComputerChoice(max) {
-    return Math.floor(Math.random() * max);
+/* 
+create a function to generate a choice for computer
+returns rock, paper or scissors
+*/
+
+function getComputerChoice(choice,computerChoice) {
+    let number = (Math.floor(Math.random() * choice));
+    if (number === 0) {
+        computerChoice = "rock";
+    } else if (number === 1) {
+        computerChoice = "paper";
+    } else if (number === 2) {
+        computerChoice = "scissors";
+    }
+    return computerChoice;
 }
  console.log(getComputerChoice(3))
 
+/*
+create a function that prompts user to input a choice
+return choice
+*/
+
+function getHumanChoice() {
+    
+}
+console.log(getHumanChoice)
+
+/*
+write variables to keep track of player score
+*/
